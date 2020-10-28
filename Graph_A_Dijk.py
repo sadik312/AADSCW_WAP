ebb2e8deeddce414c3c9cf90fbc4eccebe9483f4
PrayushU <prayushudas@gmail.com>
2020-10-28 23:33:22 +0000

    Now prints the final result

--- Graph_A_Dijk.py
+++ Graph_A_Dijk.py
@@ -12,9 +12,10 @@ def creating_graph(g):
     for i in dl.line:
 
         """For the loop the inintal node is needed witch is the start_node"""
-
+        g.add_node(dl.line[i].start_node.item[1], cum_wg=None, line=dl.line[i].start_node.item[0])
+        g.add_node(dl.line[i].start_node.item[2], cum_wg=None, line=dl.line[i].start_node.item[0])
         g.add_edge(dl.line[i].start_node.item[1], dl.line[i].start_node.item[2],
-                   weight=dl.line[i].start_node.item[3], line=dl.line[i].start_node.item[0], Sweight=0)
+                   weight=dl.line[i].start_node.item[3], line=dl.line[i].start_node.item[0])
         n = dl.line[i].start_node
 
         """while loop used to iterate through all the nodes/stations in the tube line, same technique used as 
@@ -24,8 +25,9 @@ def creating_graph(g):
             if n.next is None:
                 break
             else:
-
-                g.add_edge(n.item[1], n.item[2], weight=n.item[3], line=n.item[0], Sweight=0)
+                g.add_node(n.item[1], cum_wg=None, line=n.item[0])
+                g.add_node(n.item[2], cum_wg=None, line=n.item[0])
+                g.add_edge(n.item[1], n.item[2], weight=n.item[3], line=n.item[0])
                 n = n.next
 
 
@@ -42,11 +44,6 @@ creating_graph(graph)
 """For the dijkstra algorithm"""
 
 
-# print(graph.edges)
-# for i in graph.neighbors('Kenton'):
-#    print(i)
-# print(graph.get_edge_data('Kenton'))
-
 def dijkstra(g, src):
     d = {}
     vised = {}
@@ -54,6 +51,7 @@ def dijkstra(g, src):
     for vertex in g:
         if vertex == src:
             d[vertex] = 0
+            g.nodes[vertex]['cum_wg'] = 0
         else:
             d[vertex] = float('inf')
         heapq.heappush(heapx, (d[vertex], vertex))
@@ -67,8 +65,9 @@ def dijkstra(g, src):
                 weight = int(g.get_edge_data(u, e)['weight'])
                 if d[u] + weight < d[e]:
                     d[e] = d[u] + weight
-                    g.get_edge_data(u, e)['Sweight'] = d[e]
+                    g.nodes[e]['cum_wg'] = d[e]
                     heapq.heappush(heapx, (d[e], e))
+
 
 '''
 For testing, dw about this
@@ -92,25 +91,43 @@ def shortest(src, des):
 dijkstra(graph, 'Kenton')
 shortest('Kenton', 'Bank')
 '''
+path = []
 
 
 def shortest(src, des):
-    path = []
     station = None
     next = des
+    # heapq.heappush(path, (graph.nodes[src]['cum_wg'], src))
     while next != src:
         i = float('inf')
         for adjacent in graph.neighbors(next):
-            '''
-            print(adjacent)
-            print(graph.get_edge_data(next, adjacent)['Sweight'])
-            '''
-            if graph.get_edge_data(next, adjacent)['Sweight'] < i and adjacent not in path:
-                i = graph.get_edge_data(next, adjacent)['Sweight']
+            if graph.nodes[adjacent]['cum_wg'] < i:
+                i = graph.nodes[adjacent]['cum_wg']
                 station = adjacent
-        path.append(next)
+        # heapq.heappush(path, (graph.nodes[next]['cum_wg'], next))
+        path.append((next, graph.nodes[next]['line'], graph.nodes[next]['cum_wg']))
         next = station
-    print(path)
 
-dijkstra(graph, 'Kenton')
-shortest('Kenton', 'Bank')
+    graph.nodes[next]['line'] = graph.get_edge_data(src, path[-1][0])['line']
+    path.append((src, graph.nodes[next]['line'], graph.nodes[src]['cum_wg']))
+
+
+def display():
+    temp = None
+    while len(path) != 0:
+        if temp is None:
+            temp = path[-1]
+            print(path[-1][1])
+            print('\t- ' + path.pop(-1)[0])
+        if temp[1] != path[-1][1]:
+            print(path[-1][1])
+            temp = path[-1]
+            print('\t- ' + path.pop(-1)[0])
+        else:
+            temp = path[-1]
+            print('\t- ' + path.pop(-1)[0])
+
+
+dijkstra(graph, 'Edgware')
+shortest('Edgware', 'Morden ')
+display()
