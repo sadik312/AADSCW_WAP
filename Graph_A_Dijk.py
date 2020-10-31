e314742e34e95c44e4ce4457eb00b708ad2bdfbb
PrayushU <prayushudas@gmail.com>
2020-10-31 16:17:27 +0000

    connected code

--- Graph_A_Dijk.py
+++ Graph_A_Dijk.py
@@ -1,4 +1,4 @@
-from datetime import datetime, time
+
 import doubly_linked_list as dl
 import networkx as nx
 import heapq
@@ -8,73 +8,73 @@ graph = nx.Graph()
 
 
 def creating_graph(g):
-    """The for loop below will loop for all the values in line dict created in 'doubly_linked_list.py'
-        adding edges by iterating through every node in the line"""
-    for i in dl.line:
+	"""The for loop below will loop for all the values in line dict created in 'doubly_linked_list.py'
+		adding edges by iterating through every node in the line"""
+	for i in dl.line:
 
-        """For the loop the inintal node is needed witch is the start_node"""
-        g.add_node(dl.line[i].start_node.item[1], cum_wg=None, line=dl.line[i].start_node.item[0])
-        g.add_node(dl.line[i].start_node.item[2], cum_wg=None, line=dl.line[i].start_node.item[0])
-        g.add_edge(dl.line[i].start_node.item[1], dl.line[i].start_node.item[2],
-                   weight=dl.line[i].start_node.item[3], line=dl.line[i].start_node.item[0])
-        n = dl.line[i].start_node
+		"""For the loop the inintal node is needed witch is the start_node"""
+		g.add_node(dl.line[i].start_node.item[1].strip(), cum_wg=None, line=dl.line[i].start_node.item[0])
+		g.add_node(dl.line[i].start_node.item[2].strip(), cum_wg=None, line=dl.line[i].start_node.item[0])
+		g.add_edge(dl.line[i].start_node.item[1].strip(), dl.line[i].start_node.item[2].strip(),
+				   weight=dl.line[i].start_node.item[3], line=dl.line[i].start_node.item[0])
+		n = dl.line[i].start_node
 
-        """while loop used to iterate through all the nodes/stations in the tube line, same technique used as 
-            doubly linked list to traverse the list. 
-            Making a variable n and assigning it to start_node will allow to traverse through the list"""
-        while n is not None:
-            if n.next is None:
-                break
-            else:
-                g.add_node(n.item[1], cum_wg=None, line=n.item[0])
-                g.add_node(n.item[2], cum_wg=None, line=n.item[0])
-                g.add_edge(n.item[1], n.item[2], weight=n.item[3], line=n.item[0])
-                n = n.next
+		"""while loop used to iterate through all the nodes/stations in the tube line, same technique used as 
+			doubly linked list to traverse the list. 
+			Making a variable n and assigning it to start_node will allow to traverse through the list"""
+		while n is not None:
+			if n.next is None:
+				break
+			else:
+				g.add_node(n.item[1].strip(), cum_wg=None, line=n.item[0])
+				g.add_node(n.item[2].strip(), cum_wg=None, line=n.item[0])
+				g.add_edge(n.item[1].strip(), n.item[2].strip(), weight=n.item[3], line=n.item[0])
+				n = n.next
 
 
 """This is how the data is stored as list in the nodes for each tube line, obviously print is not necessary"""
 # print(dl.Bakerloo.traversing_the_list())
 
 """This is the format to extract the data from the graph, obvuiously print is not necessary when just using/redirecting
-    the data"""
+	the data"""
 # print(graph.get_edge_data('Harrow & Wealdstone', 'Kenton')['weight'])
 
 """For the dijkstra algorithm"""
 
 
 def dijkstra(g, src):
-    d = {}
-    vised = {}  # Maps the Visited Vertices to its 'distance to vertex' value
-    heapx = []  # Holds Heap within Array Format
-    for vertex in g:
-        if vertex == src:
-            ''' As there are no values present within the Heap,
-            The first Vertex will be initiated as the 'root' '''
-            d[vertex] = 0
-            g.nodes[vertex]['cum_wg'] = 0
-        else:
-            ''' All other Vertex's having a value of 'positive infinity' '''
-            d[vertex] = float('inf')
-        heapq.heappush(heapx, (d[vertex], vertex))
-        heapq.heapify(heapx)
-        ''' The heapify function is needed to retain heap order
-            - Containing actions such as Bubbling up-heap or down-heap'''
+	d = {}
+	vised = {}  # Maps the Visited Vertices to its 'distance to vertex' value
+	heapx = []  # Holds Heap within Array Format
+	for vertex in g:
+		if vertex == src:
+			''' As there are no values present within the Heap,
+			The first Vertex will be initiated as the 'root' '''
+			d[vertex] = 0
+			g.nodes[vertex]['cum_wg'] = 0
+		else:
+			''' All other Vertex's having a value of 'positive infinity' '''
+			d[vertex] = float('inf')
+		heapq.heappush(heapx, (d[vertex], vertex))
+		heapq.heapify(heapx)
+		''' The heapify function is needed to retain heap order
+			- Containing actions such as Bubbling up-heap or down-heap'''
 
-    ''' This loop encompasses the 'Relaxation step' found within all Dijkstra's Algorithm's'''
-    while len(heapx) != 0:
-        key, u = heapq.heappop(heapx)
-        vised[u] = key
-        ''' For all the outgoing/neighbouring edges '''
-        for e in g.neighbors(u):
-            if e not in vised:
-                weight = int(g.get_edge_data(u, e)['weight'])
-                ''' Relaxation step on edges (e, u)'''
-                '''is the accumulated value better than d[e]'''
-                if d[u] + weight < d[e]:
-                    d[e] = d[u] + weight
-                    g.nodes[e]['cum_wg'] = d[e]
-                    ''' Pushing updated distances onto heap'''
-                    heapq.heappush(heapx, (d[e], e))
+	''' This loop encompasses the 'Relaxation step' found within all Dijkstra's Algorithm's'''
+	while len(heapx) != 0:
+		key, u = heapq.heappop(heapx)
+		vised[u] = key
+		''' For all the outgoing/neighbouring edges '''
+		for e in g.neighbors(u):
+			if e not in vised:
+				weight = int(g.get_edge_data(u, e)['weight'])
+				''' Relaxation step on edges (e, u)'''
+				'''is the accumulated value better than d[e]'''
+				if d[u] + weight < d[e]:
+					d[e] = d[u] + weight
+					g.nodes[e]['cum_wg'] = d[e]
+					''' Pushing updated distances onto heap'''
+					heapq.heappush(heapx, (d[e], e))
 
 
 path = []
@@ -82,62 +82,42 @@ path = []
 
 
 def shortest(src, des):
-    station = None
-    next = des
-    # heapq.heappush(path, (graph.nodes[src]['cum_wg'], src))
-    while next != src:
-        i = float('inf')
-        for adjacent in graph.neighbors(next):
-            if graph.nodes[adjacent]['cum_wg'] < i:
-                i = graph.nodes[adjacent]['cum_wg']
-                station = adjacent
-        # heapq.heappush(path, (graph.nodes[next]['cum_wg'], next))
-        path.append((next, graph.nodes[next]['line'], graph.nodes[next]['cum_wg']))
-        next = station
+	station = None
+	next = des
+	# heapq.heappush(path, (graph.nodes[src]['cum_wg'], src))
+	while next != src:
+		i = float('inf')
+		for adjacent in graph.neighbors(next):
+			if graph.nodes[adjacent]['cum_wg'] < i:
+				i = graph.nodes[adjacent]['cum_wg']
+				station = adjacent
+		# heapq.heappush(path, (graph.nodes[next]['cum_wg'], next))
+		path.append((next, graph.nodes[next]['line'], graph.nodes[next]['cum_wg']))
+		next = station
 
-    graph.nodes[next]['line'] = graph.get_edge_data(src, path[-1][0])['line']
-    path.append((src, graph.nodes[next]['line'], graph.nodes[src]['cum_wg']))
+	graph.nodes[next]['line'] = graph.get_edge_data(src, path[-1][0])['line']
+	path.append((src, graph.nodes[next]['line'], graph.nodes[src]['cum_wg']))
 
 
 def display():
-    temp = None
-    while len(path) != 0:
-        if temp is None:
-            temp = path[-1]
-            print(path[-1][1])
-            print('\t- ' + path.pop(-1)[0])
-        if temp[1] != path[-1][1]:
-            print(path[-1][1])
-            temp = path[-1]
-            print('\t- ' + path.pop(-1)[0])
-        else:
-            temp = path[-1]
-            print('\t- ' + path.pop(-1)[0])
+	temp = None
+	while len(path) != 0:
+		if temp is None:
+			temp = path[-1]
+			print(path[-1][1])
+			print('\t- ' + path.pop(-1)[0])
+		if temp[1] != path[-1][1]:
+			if temp[2] is None:
+				pass
+			else:
+				print(str(path[-1][1]) + ' ' + str(temp[2]))
+			temp = path[-1]
+			print('\t- ' + path.pop(-1)[0])
+		else:
+			temp = path[-1]
+			print('\t- ' + path.pop(-1)[0])
+	print('Total time: {}'.format(temp[2]))
 
 
-def in_time(startTime, endTime):
-    '''Current Universal Time'''
-    cur_time = datetime.utcnow().time()
-    if startTime < endTime:
-        ''' The Current Time falls within the '''
-        return cur_time >= startTime and cur_time <= endTime
-    else:
-        ''' The 'or' takes into account if the period spans overnight'''
-        return cur_time >= startTime or cur_time <= endTime
-
-
-def main():
-    ''' Check if User is Accessing the Application within train running periods'''
-    if in_time(time(5, 00), time(0)):  # 5AM -> MIDNIGHT
-        ''' Call upon GUI'''
-        """Function creating the graph"""
-        creating_graph(graph)
-        ''' Get input and insert into Dijkstra's Algorithm'''
-        dijkstra(graph, 'Edgware')
-        shortest('Edgware', 'Morden ')
-        ''' Display within the GUI'''
-        display()
-
-
-main()
-
+"""Function creating the graph"""
+creating_graph(graph)
--- ui.py
+++ ui.py
@@ -1,50 +1,7 @@
-'''
-import tkinter
-from PIL import ImageTk, Image
-
-root = tkinter.Tk()
-root.geometry("800x500")
-root.title("Router Planner App")
-#root.iconbitmap()
-
-#entry = tkinter.Entry(root)
-#entry.pack()
-
-
-def Confirm():
-    with open("stations.txt", "r") as f: 
-        if user_input.get() in f.read(): 
-            alabel = tkinter.Label(root, text="You have confirmed " + user_input.get()).pack()
-        else: 
-            blabel = tkinter.Label(root, text="Not found").pack()
-
-user_input = tkinter.Entry(root)
-user_input.insert(0, "From")
-user_input.pack()
-
-button = tkinter.Button(root, text="Enter", command=Confirm)
-button.pack()
-
-user_input = tkinter.Entry(root)
-user_input.insert(0, "To")
-user_input.pack()
-
-button = tkinter.Button(root, text="Enter", command=Confirm)
-button.pack(pady=20)
-
-# new window for Map view:
-def map_page():
-	map_tab = Toplevel()
-	map_image = ImageTk.PhotoImage(Image.open("london_underground_map.png"))
-	map_label = Label(map_tab, image=map_image).pack()
-
-map_btn = tkinter.Button(root, text="Map", command=map_page)
-map_btn.pack(pady=25)
-
-root.mainloop()'''
-
+from datetime import datetime, time
 from tkinter import * 
 from PIL import ImageTk, Image
+import Graph_A_Dijk as gr
 
 root = Tk()
 root.title('Route Planner App')
@@ -53,28 +10,75 @@ root.geometry('800x500')
 #from website: https://www.flaticon.com/authors/freepik
 
 # func to confirm entered stations
+source = None
+destination = None
+def words(string):
+	if " " in string:
+		newText = ''
+		wap = string.split(' ')
+		for val in wap:
+			# Remove space from starting and ending
+			val = val.strip()
+
+			# Capitalize each list item and merge with '.'
+			newText += val.capitalize() + ' '
+
+			# Remove the last dot
+
+		return newText.strip()
+	else:
+		return string.capitalize()
+
+
+def in_time(startTime, endTime):
+	'''Current Universal Time'''
+	cur_time = datetime.utcnow().time()
+	if startTime < endTime:
+		''' The Current Time falls within the '''
+		return cur_time >= startTime and cur_time <= endTime
+	else:
+		''' The 'or' takes into account if the period spans overnight'''
+		return cur_time >= startTime or cur_time <= endTime
+
+
+def main():
+	''' Check if User is Accessing the Application within train running periods'''
+	if in_time(time(5, 00), time(0)):  # 5AM -> MIDNIGHT
+		''' Call upon GUI'''
+		''' Get input and insert into Dijkstra's Algorithm'''
+		gr.dijkstra(gr.graph, source)
+		gr.shortest(source, destination)
+		''' Display within the GUI'''
+
+
 def Confirm():
-	with open ('stations.txt', 'r') as file:
-			if user_input.get() and user_input1.get() in file.read():
-				confirm_label = Label(root, text="Starting Station: " + user_input.get())
-				confirm_label.pack()
-				confirm_label1 = Label(root, text="Destination Station: " + user_input1.get())
-				confirm_label1.pack()
-			
-			else:
-				error_label = Label(root, text="both stations not found")
-				error_label.pack() 
-				
-# input for starting station
-user_input = Entry(root)
-user_input.insert(0, "From:")
-user_input.pack()
+	source = words(src_input.get().lower().strip())
+	destination = words(des_input.get().lower().strip())
+
+	if source in gr.graph.nodes():
+		if destination in gr.graph.nodes():
+			confirm_label = Label(root, text="Starting Station: " + words(source))
+			confirm_label.pack()
+			confirm_label1 = Label(root, text="Destination Station: " + words(destination))
+			confirm_label1.pack()
+			main()
+		else:
+			error_label = Label(root, text="Destination not found")
+			error_label.pack()
+	else:
+		error_label = Label(root, text="Starting station not found")
+		error_label.pack()
+
+			# input for starting station
+src_input = Entry(root)
+src_input.insert(0, "Morden")
+src_input.pack()
 
 
 # input for destination station
-user_input1 = Entry(root)
-user_input1.insert(0, "To:")
-user_input1.pack()
+des_input = Entry(root)
+des_input.insert(0, "edgware")
+des_input.pack()
 
 # button to confirm station entries
 confirm_btn = Button(root, text="Confirm", command=Confirm)
@@ -82,10 +86,10 @@ confirm_btn.pack()
 
 # resets entry boxes, but not the station confirmations (can't seem to get that to work)
 def reset(): 
-	user_input.delete(0, END)
-	user_input1.delete(0, END)
-	user_input.insert(0, "From:")
-	user_input1.insert(0, "To: ")
+	src_input.delete(0, END)
+	des_input.delete(0, END)
+	src_input.insert(0, "From:")
+	des_input.insert(0, "To: ")
 	#confirm_label.delete(0, END)
 	
 
@@ -108,6 +112,7 @@ map_btn.pack()
 # Exit button: 
 exit_btn = Button(root, text="Exit", command=root.quit)
 exit_btn.pack()
+
 
 
 mainloop()
