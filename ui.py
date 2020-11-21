07c1d6818924433ba794f3656647365991083cd1
PrayushU <prayushudas@gmail.com>
2020-11-21 15:44:46 +0000

    bugs fixed and improbements, potentially final code

--- ui.py
+++ ui.py
@@ -6,12 +6,23 @@ from tkinter import messagebox
 import Graph_A_Dijk as gr
 import webbrowser
 
+def center_window(w, h, frame):
+    # get screen width and height
+    ws = frame.winfo_screenwidth()
+    hs = frame.winfo_screenheight()
+    # calculate position x, y
+    x = (ws / 2) - (w / 2)
+    y = (hs / 2) - (h / 2)
+    frame.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+
+looks = {'fg': 'white', 'bg': '#916648', 'highlightbackground': '#916648'}
 root = Tk()
 root.title('Route Planner App')
-root.geometry('800x500')
+center_window(800, 500, root)
 photo = PhotoImage(file="Photos/PR.gif")
 root.iconphoto(False, photo)
-root.configure(bg='black')
+root.configure(bg=looks['bg'])
 confirm_label = Label(root)
 confirm_label1 = Label(root)
 error_label = Label(root)
@@ -61,34 +72,60 @@ def confirm():
     global confirm_label
     global confirm_label1
     global error_label
+    global time_label
     confirm_label.destroy()
     confirm_label1.destroy()
     error_label.destroy()
+    time_label.destroy()
+
+    def validation(a, b):
+        try:
+            int(a)
+            int(a)
+            return True
+        except ValueError:
+            return False
 
     source = words(src_input.get().lower().strip())
     destination = words(des_input.get().lower().strip())
     if source != destination:
-        if (0 <= int(depart_hour.get()) <= 23) and (0 <= int(depart_min.get()) <= 59):
+        if validation(depart_hour.get(), depart_min.get()):
+            if (0 <= int(depart_hour.get()) <= 23) and (0 <= int(depart_min.get()) <= 59):
 
-            if source in gr.graph.nodes():
-                if destination in gr.graph.nodes():
-                    confirm_label = Label(root, text="Starting Station: " + words(source))
-                    confirm_label.pack()
-                    confirm_label1 = Label(root, text="Destination Station: " + words(destination))
-                    confirm_label1.pack()
-                    main()
+                if source in gr.graph.nodes():
+                    if destination in gr.graph.nodes():
+                        confirm_label = Label(root, text="Starting Station: " + words(source),
+                                              bg=looks['bg'], fg=looks['fg'],
+                                              highlightbackground=looks['highlightbackground'])
+                        confirm_label.pack()
+                        confirm_label1 = Label(root, text="Destination Station: " + words(destination),
+                                               bg=looks['bg'], fg=looks['fg'],
+                                               highlightbackground=looks['highlightbackground'])
+                        confirm_label1.pack()
+                        main()
+                    else:
+                        error_label = Label(root, text="Destination not found",
+                                            bg=looks['bg'], fg=looks['fg'],
+                                            highlightbackground=looks['highlightbackground'])
+                        error_label.pack()
                 else:
-                    error_label = Label(root, text="Destination not found")
+                    error_label = Label(root, text="Starting station not found",
+                                        bg=looks['bg'], fg=looks['fg'],
+                                        highlightbackground=looks['highlightbackground'])  ###
                     error_label.pack()
             else:
-                error_label = Label(root, text="Starting station not found")  ###
-                error_label.pack()  ###
+                error_label = Label(root, text="Time entered is invalid",
+                                    bg=looks['bg'], fg=looks['fg'],
+                                    highlightbackground=looks['highlightbackground'])  ###
+                error_label.pack()
         else:
-            error_label = Label(root, text="Time entered is invalid")  ###
-            error_label.pack()  ###
+            time_label = Label(root, text='Entered time is invalid',
+                               bg=looks['bg'], fg=looks['fg'], highlightbackground=looks['highlightbackground'])
+            time_label.pack()
     else:
-        error_label = Label(root, text="Same station")  ###
-        error_label.pack()  ###
+        error_label = Label(root, text="Same station",
+                            bg=looks['bg'], fg=looks['fg'], highlightbackground=looks['highlightbackground'])  ###
+        error_label.pack()
 
 
 def main():
@@ -112,33 +149,28 @@ def main():
 
 """ input for starting station"""
 
-src_input = Entry(root)
+src_input = Entry(root, bg=looks['bg'], fg=looks['fg'], highlightbackground=looks['highlightbackground'])
 src_input.insert(0, "From:")
 src_input.place(relx=0.5, rely=0.125, anchor=CENTER)
 
 """ input for destination station"""
-des_input = Entry(root)
+des_input = Entry(root, bg=looks['bg'], fg=looks['fg'], highlightbackground=looks['highlightbackground'])
 des_input.insert(0, "To:")
 des_input.place(relx=0.5, rely=0.2, anchor=CENTER)
 
-time_label = Label(root, text='Enter time of departure')
+time_label = Label(root, text='Enter time of departure', bg=looks['bg'], fg=looks['fg'],
+                   highlightbackground=looks['highlightbackground'])
 time_label.place(relx=0.5, rely=0.25, anchor=CENTER)
-collon = Label(root, text=":")
+collon = Label(root, text=":", bg=looks['bg'], fg=looks['fg'], highlightbackground=looks['highlightbackground'])
 collon.place(relx=0.5, rely=0.3, anchor=CENTER)
 
-depart_hour = Entry(root, width=2)
+depart_hour = Entry(root, width=2, bg=looks['bg'], fg=looks['fg'], highlightbackground=looks['highlightbackground'])
 depart_hour.insert(0, str(cur_time)[:2])
 depart_hour.place(relx=0.48, rely=0.3, anchor=CENTER)
 
-depart_min = Entry(root, width=2)
+depart_min = Entry(root, width=2, bg=looks['bg'], fg=looks['fg'], highlightbackground=looks['highlightbackground'])
 depart_min.insert(0, str(cur_time)[3:5])
 depart_min.place(relx=0.52, rely=0.3, anchor=CENTER)
-
-
-def bold(text):
-    bold_font = font.Font(root, text)
-    bold_font.configure(weight='bold')
-    Label(root, text=bold_font).pack()
 
 
 def checking_time(a, b):
@@ -149,7 +181,8 @@ def checking_time(a, b):
 
 
 """ button to confirm station entries"""
-confirm_btn = Button(root, text="Confirm", command=confirm, bg = 'red')
+confirm_btn = Button(root, text="Confirm", command=confirm,
+                     bg=looks['bg'], fg='black', highlightbackground=looks['highlightbackground'])
 confirm_btn.place(relx=0.5, rely=0.375, anchor=CENTER)
 
 """ resets entry boxes, but not the station confirmations (can't seem to get that to work)"""
@@ -167,7 +200,8 @@ def reset():
 
 """ confirm_label.delete(0, END)"""
 
-reset_btn = Button(root, text="Reset", command=reset)
+reset_btn = Button(root, text="Reset", command=reset,
+                   bg=looks['bg'], fg='black', highlightbackground=looks['highlightbackground'])
 reset_btn.place(relx=0.5, rely=0.425, anchor=CENTER)
 
 """ Button that will open up TFL Map"""
@@ -182,11 +216,13 @@ def map_page():
     map_label.pack()
 
 
-map_btn = Button(root, text="Map", command=lambda: [webbrowser.open("http://content.tfl.gov.uk/standard-tube-map.pdf")])
+map_btn = Button(root, text="Map", command=lambda: [webbrowser.open("http://content.tfl.gov.uk/standard-tube-map.pdf")],
+                 bg=looks['bg'], fg='black', highlightbackground=looks['highlightbackground'])
 map_btn.place(relx=0.5, rely=0.475, anchor=CENTER)
 
 """ Exit button:"""
-exit_btn = Button(root, text="Exit", command=root.quit)
+exit_btn = Button(root, text="Exit", command=root.quit,
+                  bg=looks['bg'], fg='black', highlightbackground=looks['highlightbackground'])
 exit_btn.place(relx=0.5, rely=0.525, anchor=CENTER)
 
 
@@ -194,12 +230,11 @@ def display_gui(time):
     gr.final = []
     global display
     display = Tk()
-    display.title('Route Planner App')
+    display.title('Route')
     if len(gr.path) < 20:
         height = 30
     else:
         height = len(gr.path) * 2
-
     text = Text(display, height=height, width=60)
     text.pack()
 
@@ -213,8 +248,8 @@ def display_gui(time):
     text.tag_configure('stations', font=('Arial', 14,))
 
     colours = {'Bakerloo': '#B36305', 'Central': '#E32017', 'Circle': '#FFD300', 'District': '#00782A',
-               'Hammersmith & City': '#F3A9BB', 'Jubilee': '#A0A5A9', 'Metropolitan': '#9B0056', 'Northern': '#000000',
-               'Piccadilly': '#003688', 'Victoria': '#0098D4', 'Waterloo & City': '#95CDBA'}
+               'Hammersmith_&_City': '#F3A9BB', 'Jubilee': '#A0A5A9', 'Metropolitan': '#9B0056', 'Northern': '#000000',
+               'Piccadilly': '#003688', 'Victoria': '#0098D4', 'Waterloo_&_City': '#95CDBA'}
 
     '''      
             if changes is None:
@@ -232,22 +267,32 @@ def display_gui(time):
                 text.insert(END, 'Final time:{}'.format(gr.cum_time(cur_time, i[2])), 'Main_station')
 
             else:
-                lines = len(i[1])
+                text.insert(END, ' ' * 5, 'stations')
 
-                text.insert(END, ' ' * 5 + '|' * lines + '-{}'.format(i[0]) + '\n', 'stations')
+                for j in i[1]:
+                    text.tag_configure(j, font=('Arial', 15, 'bold'), foreground=colours[j])
+                    text.insert(END,'|', j)
+
+                text.insert(END, '-{}'.format(i[0]) + '\n', 'stations')
 
         else:
 
             text.insert(END, i[0] + '\n', 'Main_station')
             for lines in i[1]:
-                text.tag_configure(lines, font=('Arial', 15, 'bold'), foreground=colours[lines])
-                if lines == i[1][-1]:
-                    text.insert(END, ' ' + lines, '{}'.format(lines))
+                temp1 = lines
+                if lines == 'Hammersmith & City':
+                    temp1 = 'Hammersmith_&_City'
+                if lines == 'Waterloo & City':
+                    temp1 = 'Waterloo_&_City'
+
+                text.tag_configure(temp1, font=('Arial', 15, 'bold'), foreground=colours[temp1])
+                if temp1 == i[1][-1]:
+                    text.insert(END, ' ' + temp1, '{}'.format(temp1))
                     text.insert(END, ' ' + gr.cum_time(cur_time, i[2]) + '\n', 'bold')
-                elif lines == i[1][0]:
-                    text.insert(END, ' ' * 7 + lines + ',', '{}'.format(lines))
+                elif temp == i[1][0]:
+                    text.insert(END, ' ' * 7 + temp1 + ',', '{}'.format(temp1))
                 else:
-                    text.insert(END, lines + ',', '{}'.format(lines))
+                    text.insert(END, temp1 + ',', '{}'.format(temp1))
         temp = i[1]
 
 
